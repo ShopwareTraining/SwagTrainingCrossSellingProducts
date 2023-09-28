254548cae05f17207abccbaffd18b4b8b9bfd06f
Jisse Reitsma <jisse@yireo.com>
2023-09-28 11:04:39 +0200

    Add event subscriber; Beautify Twig

--- src/Event/AddCrossSellingProductsToProductPageSubscriber.php
+++ src/Event/AddCrossSellingProductsToProductPageSubscriber.php
@@ -6,19 +6,27 @@ use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepository;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Shopware\Storefront\Page\Navigation\NavigationPageLoadedEvent;
 use Shopware\Storefront\Page\Product\ProductPageLoadedEvent;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-//#[AsEventListener(event: ProductPageLoadedEvent::class)]
-class AddCrossSellingProductsToProductPageListener
+class AddCrossSellingProductsToProductPageSubscriber implements EventSubscriberInterface
 {
     public function __construct(
         #[Autowire(service: 'sales_channel.product.repository')] private SalesChannelRepository $productRepository,
         private SystemConfigService $systemConfigService
     ) {}
 
-    public function __invoke(ProductPageLoadedEvent $event)
+    public static function getSubscribedEvents()
+    {
+        return [
+            ProductPageLoadedEvent::class => 'addProductsToProductPage'
+        ];
+    }
+
+    public function addProductsToProductPage(ProductPageLoadedEvent $event)
     {
         $productIds = $this->getProductIdsFromConfig($event);
         $products = $this->getProductsFromProductIds($event, $productIds);
