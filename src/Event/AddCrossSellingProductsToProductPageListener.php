898fd2f38a83ef847c2e99479f4a261b2aec660e
Jisse Reitsma <jisse@yireo.com>
2023-09-28 10:19:56 +0200

    PoC of page loader with configuration injected

--- src/Event/AddCrossSellingProductsToProductPageListener.php
+++ src/Event/AddCrossSellingProductsToProductPageListener.php
@@ -2,12 +2,25 @@
 
 namespace SwagTraining\CrossSellingProducts\Event;
 
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Storefront\Page\Product\ProductPageLoadedEvent;
 
 class AddCrossSellingProductsToProductPageListener
 {
+    public function __construct(
+        private SystemConfigService $systemConfigService
+    ) {}
+
     public function addDataToProductPage(ProductPageLoadedEvent $event)
     {
-        $event->getPage()->addArrayExtension('sdfsd', ['sdfsd' => 'sdffs']);
+        $salesChannelId = $event->getSalesChannelContext()->getSalesChannelId();
+        $productIds = $this->systemConfigService->get('SwagTrainingCrossSellingProducts.config.products', $salesChannelId);
+        $products = ['fsdf' => $productIds];
+
+        $event->getPage()->addArrayExtension('crossSellingProducts', $products);
     }
 }
