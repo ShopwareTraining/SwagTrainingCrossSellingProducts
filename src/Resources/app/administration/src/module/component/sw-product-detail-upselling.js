3f6f6eac95937335510e8dcf9dbea097d214d034
jisse Reitsma <jisse@yireo.com>
2023-10-08 14:01:18 +0200

    First draft of loading and saving entities properly

--- src/Resources/app/administration/src/module/component/sw-product-detail-upselling.js
+++ src/Resources/app/administration/src/module/component/sw-product-detail-upselling.js
@@ -1,41 +1,84 @@
 import template from './sw-product-detail-upselling.html.twig'
 
+const {mapState} = Shopware.Component.getComponentHelper();
+const {Context} = Shopware;
+const {Criteria} = Shopware.Data;
+
 Shopware.Component.register('sw-product-detail-upselling', {
     data() {
-      return {
-          context: null,
-          productIds: [],
-          criteria: null,
-          repository: null
-      }
+        return {
+            context: null,
+            upsellingProducts: new Map,
+            criteria: null,
+            productRepository: null,
+            productUpsellingRepository: null,
+        }
     },
     inject: ['repositoryFactory', 'systemConfigApiService'],
 
     computed: {
-        getProductIds() {
-            console.log('Product IDs', this.productIds);
-            return this.productIds.join(', ');
+        ...mapState('swProductDetail', [
+            'product',
+        ]),
+
+        hasUpsellingProductIds() {
+           return this.upsellingProducts && this.upsellingProducts.length > 0;
+        },
+
+        upsellingProductIds() {
+            console.log('upsellingProductIds', this.upsellingProducts);
+            return Array.from(this.upsellingProducts.keys());
         }
     },
 
     methods: {
-        updateProductIds(values) {
-            this.productIds = values;
+        async updateUpsellingProductIds(newUpsellingProductIds) {
+            this.upsellingProducts.forEach(async (upsellingEntity, upsellingProductId, map) => {
+                await this.productUpsellingRepository.delete([upsellingEntity.id]);
+            });
+
+            newUpsellingProductIds.forEach(async newUpsellingProductId => {
+                let upsellingEntity = this.productUpsellingRepository.create(Context.api);
+                upsellingEntity.productId = this.product.id;
+                upsellingEntity.upsellingProductId = newUpsellingProductId;
+                await this.productUpsellingRepository.save(upsellingEntity, Shopware.Context.api);
+            });
+
+            this.loadUpsellingProducts();
+        },
+
+        async loadUpsellingProducts() {
+            if (!this.product || !this.product.extensions || !this.product.extensions.upsellingProducts) {
+                return [];
+            }
+
+            const criteria = new Criteria();
+            criteria.addFilter(Criteria.equals('productId', this.product.id));
+
+            const upsellingProducts = new Map();
+            await this.productUpsellingRepository.search(criteria, {...Context.api, inheritance: true}).then(
+                (upsellingEntities) => {
+                    upsellingEntities.forEach(upsellingEntity => {
+                        upsellingProducts.set(upsellingEntity.upsellingProductId, upsellingEntity);
+                    });
+                });
+
+            this.upsellingProducts = upsellingProducts;
         }
     },
 
     template,
 
     created() {
-        this.systemConfigApiService.getValues('SwagTrainingUpsellingProducts.config.products')
-            .then((values) => {
-                Object.keys(values).forEach((key) => {
-                    this.productIds.push(values[key]);
-                });
-            });
-
-        this.context = { ...Shopware.Context.api };
-        this.criteria  = new Shopware.Data.Criteria();
-        this.repository = this.repositoryFactory.create('product');
+        this.context = {...Shopware.Context.api};
+        this.criteria = new Shopware.Data.Criteria();
+        this.productRepository = this.repositoryFactory.create('product');
+        this.productUpsellingRepository = this.repositoryFactory.create('product_upselling');
     },
+
+    watch: {
+        product() {
+            this.loadUpsellingProducts();
+        }
+    }
 })
